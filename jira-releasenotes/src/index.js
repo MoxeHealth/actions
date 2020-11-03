8de3040234d477a7b041ca13e5ab8f9cfe7a644a
blobrutto <ben@moxehealth.com>
2020-11-03 15:03:17 -0800

    Add back core dependencies

--- jira-releasenotes/src/index.js
+++ jira-releasenotes/src/index.js
@@ -1,19 +1,14 @@
-//const core = require('@actions/core');
+const core = require('@actions/core');
 const { checkEnv, run } = require('../../utils');
 const { createReleaseNotes } = require('./jira-releasenotes');
 
 run(async () => {
   checkEnv(['JIRA_USERNAME', 'JIRA_PASSWORD']);
 
-  //const protocol = core.getInput('jira-protocol', { required: true });
-  //const host = core.getInput('jira-host', { required: true });
-  //const projectKey = core.getInput('jira-project', { required: true });
-  //const releaseNoteField = core.getInput('field-releasenote', { required: true });
-  
-  const protocol = 'https';
-  const host = 'moxehealth.atlassian.net';
-  const projectKey = 'PUR';
-  const releaseNoteField = 'Release-note';
+  const protocol = core.getInput('jira-protocol', { required: true });
+  const host = core.getInput('jira-host', { required: true });
+  const projectKey = core.getInput('jira-project', { required: true });
+  const releaseNoteField = core.getInput('field-releasenote', { required: true });
   
   await createReleaseNotes({
     protocol,
